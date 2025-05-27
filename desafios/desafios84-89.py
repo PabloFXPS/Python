8451488c777b48674dba8533ff74def7bc52a5e6
PabloFXPS <pablosantiago2003@gmail.com>
2025-05-27 17:24:41 -0300

    ex 85

--- desafios/desafios84-89.py
+++ desafios/desafios84-89.py
@@ -29,4 +29,20 @@ for i,v in enumerate(cadastro):
     if v[1] == min(pesos):
         print (v[0], end=" ")'''
 
-# Exercício 85 - Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
+# Exercício 85 - Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares.
+# No final, mostre os valores pares e ímpares em ordem crescente.
+'''valores = [[],[]]
+valor = 0
+for cont in range(1,8):
+    valor = int(input(f"Digite o {cont}° numero: "))
+    if valor % 2==0: 
+        valores[0].append(valor)
+    else:
+        valores[1].append(valor)
+print("-="*30)
+valores[0].sort
+valores[1].sort
+print(f"\nPares {valores[0]}.\nImpares {valores[1]}.")'''
+
+# Exercício 86 - Crie um programa que declare uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado.
+# No final, mostre a matriz na tela, com a formatação correta.
