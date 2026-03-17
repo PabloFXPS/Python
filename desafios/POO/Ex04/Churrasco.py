3ff2b32cd7104dd2c5bc859dca8b7ac8908e3f1b
PabloFXPS <pablosantiago2003@gmail.com>
2026-03-17 17:56:51 -0400

    EX04-EX05

--- desafios/POO/Ex04/Churrasco.py
+++ desafios/POO/Ex04/Churrasco.py
@@ -1,3 +1,27 @@
 # Crie a classe Churrasco, onde seja possível informar quantas pessoas vão participar
 #e mostre quanto de carne deve ser comprado, o custo total do churrasco e o preço por pessoa.
 #Considere: Consumo padrão: 400g por pessoa e preço R$82,40/kg
+
+from rich import print
+from rich.panel import  Panel
+
+class Churrasco:
+    def __init__(self,quantidade):
+        self.quantidade = quantidade
+        self.preco = 82.40
+        self.consumo = 0.4 #kg
+
+    def comprar_carne(self):
+        return self.quantidade * self.consumo
+    def custo_total(self):
+        return self.comprar_carne() * self.preco
+    def preco_por_pessoa(self):
+        return self.custo_total() / self.quantidade
+
+    def analisar(self):
+        tabela = Panel(f"[red]Total de carne Kg por pessoa: {self.comprar_carne():,.1f}kg[/red]\n[blue]Preço total: R${self.custo_total():,.2f}[/blue]\n"
+                       f"Preço por pessoa: R${self.preco_por_pessoa():,.2f}",title=f"Churrasco para {self.quantidade} pessoas",width=40)
+        return tabela
+
+c1 = Churrasco(int(input("Quantos pessoas vão para o churrasco? ")))
+print(c1.analisar())
--- desafios/POO/Ex05/Livro.py
+++ desafios/POO/Ex05/Livro.py
@@ -1,1 +1,35 @@
 #Crie a classe Livro, que simular a passagem de páginas de um livro, considerando também se o usuário chegou ao fim da leitura.
+from time import sleep
+from rich import print
+class Livro:
+    def __init__(self, nome,quantidade_paginas):
+        self.nome = nome
+        self.quantidade_paginas = quantidade_paginas
+        self.pagina_atual=1
+
+        print(f"Abrindo...")
+        sleep(1)
+        print(f"Você esta na pagina 1 de {self.quantidade_paginas} do livro {self.nome}.")
+
+    def avancar_paginas(self, passar=1):
+        cont = 0
+        for pg in range(0,passar,1):
+            if not self.final():
+                self.pagina_atual+=1
+                sleep(0.5)
+                print(f"Pag{self.pagina_atual}",end=" > ")
+                cont+=1
+        print(f"Você agora esta na pagina > {self.pagina_atual} de {self.quantidade_paginas}.")
+        if self.final():
+            print("Chegou ao final do livro.")
+            sleep(1)
+            print("Fechando...")
+
+    def final(self)->bool:
+        return True if self.pagina_atual==self.quantidade_paginas else False
+
+
+l1 = Livro("Mentes Ansiosas", 40)
+l1.avancar_paginas(20)
+l1.avancar_paginas(10)
+l1.avancar_paginas(20)
